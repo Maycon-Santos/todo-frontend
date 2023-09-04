d5466a8c5549dbfec6cc7f99af95f7157259811c
Maycon-Santos <mayconsantos.dev@gmail.com>
2023-09-04 11:51:36 -0300

    improve service is-authenticated

--- src/services/is-authenticated.ts
+++ src/services/is-authenticated.ts
@@ -2,8 +2,12 @@ import { cookies } from 'next/headers'
 
 export default async function IsAuthenticated() {
   const cookieStore = cookies()
+  const isServer = typeof window === 'undefined'
+  const url = isServer
+    ? `${process.env.API_BASE_URL}/is_authenticated`
+    : `/api/is_authenticated`
 
-  const response = await fetch(`${process.env.HOST}/api/is_authenticated`, {
+  const response = await fetch(url, {
     headers: {
       'Content-Type': 'application/json',
       Token: cookieStore.get('token')?.value || '',
